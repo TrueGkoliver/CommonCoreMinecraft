93a63bd3593f2e0c38d09b8895691e14c694c530
Gkoliver <37561701+TrueGkoliver@users.noreply.github.com>
2020-07-12 15:15:34 -0500

    basic stuff

--- CommonCoreJava.java
+++ CommonCoreJava.java
@@ -1,6 +1,6 @@
 public class CommonCoreJava {
 	/**
-		@author Gkoliver (original author), user8399 (optimization), ect (ect)
+		@author Gkoliver (original author), SmellyModder (optimization)
 		This gets the other position of a 2-height block with 6-way orientation.
 		@param posIn The blockpos in question.
 		@param isTop Whether or not the block is the top half.
