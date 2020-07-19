4cf4dad33315acb04425f8859fc7cc669ad83990
Gkoliver <37561701+TrueGkoliver@users.noreply.github.com>
2020-07-19 13:31:06 -0500

    Added a lang file population method

--- python/main/CommonCorePython.py
+++ python/main/CommonCorePython.py
@@ -5,6 +5,8 @@
 #@return NONE
 #@import json
 #@import os
+#@important THIS WILL REPLACE ALL OF THE FILES IN THE FILEPATH.
+#Do NOT put this into your blockstates folder, it will replace all of it. 
 
 def populateLootTables(filepath, modid):
     for filename in os.listdir(filepath):
@@ -34,3 +36,21 @@ def populateLootTables(filepath, modid):
             ltable_file = open(filepath+"\\"+filename, "w")
             json.dump(ltable_file, ltable, indent=4)
             ltable_file.close()
+#@author Gkoliver (original author)
+#Populates a lang file based off of blockstate files.
+#@param filepath The filepath of the folder with blockstates. Remember, always add an extra backslash to every backslash!
+#@param modid The modid. Obviously.
+#@param the file name of the lang file (keep the .json in it) relative to the lang file.
+#@return NONE
+#@import json
+#@import os
+def populateLangfile(filepath, modid, en_us):
+    data = {}
+    for filename in os.listdir(filepath):
+        if filename.endswith(".json"):
+            truename = filename.replace(".json","")
+            truename = "block."+modid+"."+truename
+            data[truename] = " "
+    file = open(filepath+"\\"+en_us, "w")
+    json.dump(data, file, indent=4)
+    file.close()
